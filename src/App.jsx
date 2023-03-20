8bce9171a909d504ab925c8d1b00131e20a683e6
Nathanael-R <nathanielwealth20@gmail.com>
2023-03-20 12:39:22 +0000

    Set sockets to effects

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,25 @@ import { useEffect, useState } from "react";
 import Navbar from "./components/navbar/Navbar";
 import Card from "./components/card/Card";
 import { posts } from "./data";
-import { io } from "socket.io-client"
+import { io } from "socket.io-client";
 function App() {
   const [userName, setUserName] = useState("");
   const [user, setUser] = useState("");
+  const [socket, setSocket] = useState(null);
+  useEffect(() => {
+    setSocket(io.connect("http://192.168.43.87:5000"));
+  }, []);
 
   useEffect(() => {
-    const socket = io.connect("http://192.168.43.87:5000");
-    console.log(socket.on("firstEvent", (msg) => {
-      console.log(msg)
-    }))
-  }, [])
+    socket.emit("newUser", user);
+  }, [socket, user]);
   return (
     <div className="container">
       {user ? (
         <>
-          <Navbar />
+          <Navbar socket={socket}/>
           {posts.map((post) => {
-            return <Card key={post.id} post={post} />;
+            return <Card key={post.id} post={post} socket={socket} user={user}/>;
           })}
           <span className="username">{user}</span>
         </>
